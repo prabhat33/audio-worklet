883ce09e53038ab26a11c4a52a835acd45579e0d
Prabhat giri | Mackbook Air <giri.prabhat33@gmail.com>
2020-02-22 12:49:10 +0530

    changed

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var app = new express();
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')))
 app.use("/audio-worklet",express.static(path.join(__dirname, 'public')));
-app.listen(8080,()=>{
-    console.log("App started on 8080");
-})
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running at',process.env.PORT || 4000);
+});
