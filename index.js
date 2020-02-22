a1b28a90e082dcab431ae66c57aa99c97f21b2b8
Prabhat giri | Mackbook Air <giri.prabhat33@gmail.com>
2020-02-22 13:54:00 +0530

    Files added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = new express();
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')))
-app.use("/audio-worklet",express.static(path.join(__dirname, 'public')));
+app.use("/audio-worklet",express.static(path.join(__dirname, 'public/audioworklet.html')));
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running at',process.env.PORT || 4000);
 });
