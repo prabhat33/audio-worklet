3bd9e3e3b1cdfd6409e128f14500b4342e90ae7f
Prabhat Kumar Giri <1641565@TCS.com>
2020-02-24 15:26:10 +0530

    new changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')))
 app.use("/audio-worklet",express.static(path.join(__dirname, 'public/audioworklet.html')));
 app.use("/latency",express.static(path.join(__dirname, 'public/latency.html')));
+app.use("/compare",express.static(path.join(__dirname, 'public/compare.html')));
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running at',process.env.PORT || 4000);
 });
